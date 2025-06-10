10acf2c8c38e3493befe871c848acd039e164c9c
Muhamad Riduan <119645560+RduaneD@users.noreply.github.com>
2025-06-10 07:51:07 +0700

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -19,7 +19,7 @@ export default function Register() {
     }
 
     try {
-      await axios.post("https://hydrosmart-backend-production.up.railway.app/register", {
+      await axios.post("hydro-backend-production.up.railway.app/register", {
         name: nama,
         email,
         password
